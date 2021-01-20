76e05b40b7adb084fc7d44ea31b42c3ff095ddef
Stephen Coleman <stcolema@tcd.ie>
2021-01-20 12:12:47 +0000

    R markdown example.

--- Examples/mod1MetopolisGibbs.R
+++ Examples/mod1MetopolisGibbs.R
@@ -13,283 +13,286 @@ library(magrittr)
 library(pheatmap)
 library(mcclust)
 library(mclust)
-# library(BatchMixtureModel)
+library(BatchMixtureModel)
 library(ggplot2)
 setMyTheme()
 
 # Rcpp::sourceCpp('Model ideas/model_metropolis_hastings.cpp')
 
-transition_kernel <- function(curr_theta, proposed_width = 0.5) {
-  n_param <- length(curr_theta)
-  new_theta <- rep(0, n_param)
-  for (i in 1:n_param) {
-    new_theta[i] <- rnorm(1, curr_theta[i], sd = proposed_width)
-  }
-  new_theta
-}
-
-transition_std_dev <- function(curr_sd, proposed_width = 0.5) {
-  n_param <- length(curr_sd)
-  new_sd <- rep(0, n_param)
-  for (i in 1:n_param) {
-    new_sd[i] <- max(0.1, rnorm(1, curr_sd[i], sd = proposed_width))
-  }
-  new_sd
-}
-
-normal_ll <- function(x, mean = 0, sd = 1) {
-  -0.5 * (2 * pi * sd**2 + (x - mean)**2 / sd**2)
-}
-
-my_kernel <- function(X, c, b, mu, sigma, K, a, s, B, N_k, N_b,
-                      mu0 = 0,
-                      gamma = 1,
-                      xi = 1,
-                      lambda = 1,
-                      delta = 1,
-                      epsilon = 1,
-                      theta = 1) {
-  N <- length(X)
-
-  part_1 <- sum((N_k + gamma - 0.5) * log(sigma)) +
-    sum((N_b + gamma - 0.5) * log(s))
-
-  exp1 <- 0
-  for (n in 1:N) {
-    c_n <- c[n]
-    b_n <- b[n]
-    exp1 <- exp1 + 1 / (sigma[c_n] * s[b_n]) * (X[n] - (mu[c_n] + a[b_n]))**2
-  }
-
-  exp2 <- lambda * sum(((mu - mu0)**2 + 2 * epsilon) / sigma)
-  exp3 <- delta * sum(((a - xi)**2 + 2 * theta) / s)
-
-  part_2 <- -0.5 * (exp1 + exp2 + exp3)
-
-  score <- part_1 + part_2
-
-  score
-}
-
-
-# my_sampler <- function(X, R, thin, K, c_init, b){
-#
-#   N <- length(X)
-#   # P <- ncol(X)
-#   B <- length(unique(b))
-#
-#   mu0 <- mean(X)
-#   lambda <- 0.2
-#   gamma <- 20
-#   epsilon <- 4
-#   xi <- 0
-#   delta <- 1
-#   rho <- 8
-#   theta <- 4
-#
-#   std_dev <- rgamma(K, gamma, epsilon)
-#   mu <- rep(0, K)
-#   for(k in 1:K){
-#     mu[k] <- rnorm(1, mu0, (1/lambda) * std_dev[k])
+# transition_kernel <- function(curr_theta, proposed_width = 0.5) {
+#   n_param <- length(curr_theta)
+#   new_theta <- rep(0, n_param)
+#   for (i in 1:n_param) {
+#     new_theta[i] <- rnorm(1, curr_theta[i], sd = proposed_width)
 #   }
-#   s <- rgamma(B, rho, theta)
-#
-#   a <- rep(0, K)
-#   for(i in 1:B){
-#     a[i] <- rnorm(1, xi, (1/delta) * s[i])
-#   }
-#
-#   N_k <- table(c_init)
-#   N_b <- table(b)
-#   c <- c_init
-#
-#   curr_score <- my_kernel(X, c, b, mu, std_dev, K, a, s, B, N_k, N_b,
-#             mu0 = mu0,
-#             gamma = gamma,
-#             xi = xi,
-#             lambda = lambda,
-#             delta = delta,
-#             epsilon = epsilon,
-#             theta = theta)
-#
-#   mu_saved <- vector("list", R)
-#   a_saved <- vector("list", R)
-#   std_dev_saved <- vector("list", R)
-#   s_saved <- vector("list", R)
-#   c_saved <- vector("list", R)
-#   weights_saved <- vector("list", R)
-#   accepted <- rep(0, R)
-#   acceptance_probs <- rep(0, R)
-#
-#   for(r in 1:R){
-#     new_mu <- transition_kernel(mu)
-#     new_a <- transition_kernel(a)
-#     new_std_dev <- transition_std_dev(std_dev)
-#     new_s <- transition_std_dev(s)
-#
-#     # detailed_balance_part <- normal_ll(mu, mean = new_mu, sd = 0.5) +
-#     #   normal_ll(a, mean = new_a, sd = 0.5) +
-#     #   normal_ll(std_dev, mean = new_std_dev, sd = 0.5) +
-#     #   normal_ll(s, mean = new_s, sd = 0.5) -
-#     #   (normal_ll(new_mu, mean = mu, sd = 0.5) +
-#     #      normal_ll(new_a, mean = a, sd = 0.5) +
-#     #      normal_ll(new_std_dev, mean = std_dev, sd = 0.5) +
-#     #      normal_ll(new_s, mean = s, sd = 0.5))
-#
-#     print(new_mu)
-#     print(new_std_dev)
-#     print(new_a)
-#     print(new_s)
-#
-#     new_score <- my_kernel(X, c, b, new_mu, new_std_dev, K, new_a, new_s, B, N_k, N_b,
-#        mu0 = mu0,
-#        gamma = gamma,
-#        xi = xi,
-#        lambda = lambda,
-#        delta = delta,
-#        epsilon = epsilon,
-#        theta = theta
-#     )
-#
-#     # detailed_balance_part <- 0
-#     # for(k in 1:K){
-#     #   detailed_balance_part <- detailed_balance_part +
-#     #     normal_ll(mu[k], mean = new_mu[k], sd = 0.5) +
-#     #     normal_ll(std_dev[k], mean = new_std_dev[k], sd = 0.5) -
-#     #     normal_ll(new_mu[k], mean = mu[k], sd = 0.5) -
-#     #     normal_ll(new_std_dev[k], mean = std_dev[k], sd = 0.5)
-#     # }
-#     #
-#     # for(b in 1:B){
-#     #   detailed_balance_part <- detailed_balance_part +
-#     #     normal_ll(a[b], mean = new_a[b], sd = 0.5) +
-#     #     normal_ll(s[b], mean = new_s[b], sd = 0.5) -
-#     #     (normal_ll(new_a[b], mean = a[b], sd = 0.5) +
-#     #        normal_ll(new_s[b], mean = s[b], sd = 0.5))
-#     # }
-#     #
-#     # print(new_score)
-#     # print(curr_score)
-#     # print(detailed_balance_part)
-#
-#     acceptance_score <- new_score - curr_score #+ detailed_balance_part
-#     acceptance_prob <- exp(acceptance_score)
-#
-#     print(acceptance_prob)
-#
-#     u <- runif(1, 0, 1)
-#     if(u < acceptance_prob){
-#       mu <- new_mu
-#       a <- new_a
-#       std_dev <- new_std_dev
-#       s  <- new_s
-#       accepted[r] <- 1
-#     }
-#
-#     weights <- N_k / sum(N_k)
-#     N_k <- rep(0, K)
-#     for(n in 1:N){
-#       x <- X[n]
-#       b_n <- b[n]
-#       probs <- rep(0,K)
-#       for(k in 1:K){
-#         probs[k] <- weights[k] * (dnorm(x, mean = mu[k] + a[b_n], sd = std_dev[k] * s[b_n]) + 1e-12)
-#       }
-#
-#       probs <- probs/sum(probs)
-#
-#       u <- runif(1, 0, 1)
-#       c[n] <- c_n <- 1 + sum(u > cumsum(probs))
-#       N_k[c_n] <- N_k[c_n] + 1
-#
-#     }
-#
-#     mu_saved[[r]] <- mu
-#     a_saved[[r]] <- a
-#     std_dev_saved[[r]] <- std_dev
-#     s_saved[[r]] <- s
-#     c_saved[[r]] <- c
-#     weights_saved[[r]] <- weights
-#     acceptance_probs[r] <- acceptance_prob
-#   }
-#
-#   list(
-#     "mu" = mu_saved,
-#     "a" = a_saved,
-#     "std_dev" = std_dev_saved,
-#     "s" = s_saved,
-#     "c" = c_saved,
-#     "weights" = weights_saved,
-#     "accepted" = accepted,
-#     "acceptance_probs" = acceptance_probs
-#   )
-#
+#   new_theta
 # }
-
-
-generateBatchData <- function(N, P, cluster_means, std_dev, batch_shift, batch_var, cluster_weights, batch_weights,
-                              row_names = paste0("Person_", 1:n),
-                              col_names = paste0("Gene_", 1:p)) {
-
-  # The number of distirbutions to sample from
-  K <- length(cluster_means)
-
-  B <- length(batch_shift)
-
-  # The membership vector for the n points
-  cluster_IDs <- sample(K, N, replace = T, prob = cluster_weights)
-
-  batch_IDs <- sample(1:B, N, replace = T, prob = batch_weights)
-
-  # The data matrix
-  my_data <- my_corrected_data <- matrix(nrow = N, ncol = P)
-
-  # Iterate over each of the columns permuting the means associated with each
-  # label.
-  for (p in 1:P)
-  {
-    reordered_cluster_means <- sample(cluster_means)
-    reordered_std_devs <- sample(std_dev)
-
-    # Draw n points from the K univariate Gaussians defined by the permuted means.
-    for (n in 1:N) {
-
-      # point_mean <- reordered_cluster_means[cluster_IDs[n]] + batch_shift[batch_IDs[n]]
-      # point_sd <- std_dev * batch_var
-      
-      x <- rnorm(1)
-
-      my_corrected_data[n, p] <- x * reordered_std_devs[cluster_IDs[n]] + reordered_cluster_means[cluster_IDs[n]]
-      my_data[n, p] <- x * reordered_std_devs[cluster_IDs[n]] * batch_var[batch_IDs[n]] + reordered_cluster_means[cluster_IDs[n]] + batch_shift[batch_IDs[n]]
-    }
-  }
-
-  # Order based upon allocation label
-  row_order <- order(cluster_IDs)
-
-  # Assign rownames and column names
-  rownames(my_data) <- row_names
-  colnames(my_data) <- col_names
-
-  rownames(my_corrected_data) <- row_names
-  colnames(my_corrected_data) <- col_names
-
-  # Return the data and the allocation labels
-  list(
-    data = my_data[row_order, ],
-    corrected_data = my_corrected_data[row_order, ],
-    cluster_IDs = cluster_IDs[row_order],
-    batch_IDs = batch_IDs[row_order]
-  )
-}
+# 
+# transition_std_dev <- function(curr_sd, proposed_width = 0.5) {
+#   n_param <- length(curr_sd)
+#   new_sd <- rep(0, n_param)
+#   for (i in 1:n_param) {
+#     new_sd[i] <- max(0.1, rnorm(1, curr_sd[i], sd = proposed_width))
+#   }
+#   new_sd
+# }
+# 
+# normal_ll <- function(x, mean = 0, sd = 1) {
+#   -0.5 * (2 * pi * sd**2 + (x - mean)**2 / sd**2)
+# }
+# 
+# my_kernel <- function(X, c, b, mu, sigma, K, a, s, B, N_k, N_b,
+#                       mu0 = 0,
+#                       gamma = 1,
+#                       xi = 1,
+#                       lambda = 1,
+#                       delta = 1,
+#                       epsilon = 1,
+#                       theta = 1) {
+#   N <- length(X)
+# 
+#   part_1 <- sum((N_k + gamma - 0.5) * log(sigma)) +
+#     sum((N_b + gamma - 0.5) * log(s))
+# 
+#   exp1 <- 0
+#   for (n in 1:N) {
+#     c_n <- c[n]
+#     b_n <- b[n]
+#     exp1 <- exp1 + 1 / (sigma[c_n] * s[b_n]) * (X[n] - (mu[c_n] + a[b_n]))**2
+#   }
+# 
+#   exp2 <- lambda * sum(((mu - mu0)**2 + 2 * epsilon) / sigma)
+#   exp3 <- delta * sum(((a - xi)**2 + 2 * theta) / s)
+# 
+#   part_2 <- -0.5 * (exp1 + exp2 + exp3)
+# 
+#   score <- part_1 + part_2
+# 
+#   score
+# }
+# 
+# 
+# # my_sampler <- function(X, R, thin, K, c_init, b){
+# #
+# #   N <- length(X)
+# #   # P <- ncol(X)
+# #   B <- length(unique(b))
+# #
+# #   mu0 <- mean(X)
+# #   lambda <- 0.2
+# #   gamma <- 20
+# #   epsilon <- 4
+# #   xi <- 0
+# #   delta <- 1
+# #   rho <- 8
+# #   theta <- 4
+# #
+# #   std_dev <- rgamma(K, gamma, epsilon)
+# #   mu <- rep(0, K)
+# #   for(k in 1:K){
+# #     mu[k] <- rnorm(1, mu0, (1/lambda) * std_dev[k])
+# #   }
+# #   s <- rgamma(B, rho, theta)
+# #
+# #   a <- rep(0, K)
+# #   for(i in 1:B){
+# #     a[i] <- rnorm(1, xi, (1/delta) * s[i])
+# #   }
+# #
+# #   N_k <- table(c_init)
+# #   N_b <- table(b)
+# #   c <- c_init
+# #
+# #   curr_score <- my_kernel(X, c, b, mu, std_dev, K, a, s, B, N_k, N_b,
+# #             mu0 = mu0,
+# #             gamma = gamma,
+# #             xi = xi,
+# #             lambda = lambda,
+# #             delta = delta,
+# #             epsilon = epsilon,
+# #             theta = theta)
+# #
+# #   mu_saved <- vector("list", R)
+# #   a_saved <- vector("list", R)
+# #   std_dev_saved <- vector("list", R)
+# #   s_saved <- vector("list", R)
+# #   c_saved <- vector("list", R)
+# #   weights_saved <- vector("list", R)
+# #   accepted <- rep(0, R)
+# #   acceptance_probs <- rep(0, R)
+# #
+# #   for(r in 1:R){
+# #     new_mu <- transition_kernel(mu)
+# #     new_a <- transition_kernel(a)
+# #     new_std_dev <- transition_std_dev(std_dev)
+# #     new_s <- transition_std_dev(s)
+# #
+# #     # detailed_balance_part <- normal_ll(mu, mean = new_mu, sd = 0.5) +
+# #     #   normal_ll(a, mean = new_a, sd = 0.5) +
+# #     #   normal_ll(std_dev, mean = new_std_dev, sd = 0.5) +
+# #     #   normal_ll(s, mean = new_s, sd = 0.5) -
+# #     #   (normal_ll(new_mu, mean = mu, sd = 0.5) +
+# #     #      normal_ll(new_a, mean = a, sd = 0.5) +
+# #     #      normal_ll(new_std_dev, mean = std_dev, sd = 0.5) +
+# #     #      normal_ll(new_s, mean = s, sd = 0.5))
+# #
+# #     print(new_mu)
+# #     print(new_std_dev)
+# #     print(new_a)
+# #     print(new_s)
+# #
+# #     new_score <- my_kernel(X, c, b, new_mu, new_std_dev, K, new_a, new_s, B, N_k, N_b,
+# #        mu0 = mu0,
+# #        gamma = gamma,
+# #        xi = xi,
+# #        lambda = lambda,
+# #        delta = delta,
+# #        epsilon = epsilon,
+# #        theta = theta
+# #     )
+# #
+# #     # detailed_balance_part <- 0
+# #     # for(k in 1:K){
+# #     #   detailed_balance_part <- detailed_balance_part +
+# #     #     normal_ll(mu[k], mean = new_mu[k], sd = 0.5) +
+# #     #     normal_ll(std_dev[k], mean = new_std_dev[k], sd = 0.5) -
+# #     #     normal_ll(new_mu[k], mean = mu[k], sd = 0.5) -
+# #     #     normal_ll(new_std_dev[k], mean = std_dev[k], sd = 0.5)
+# #     # }
+# #     #
+# #     # for(b in 1:B){
+# #     #   detailed_balance_part <- detailed_balance_part +
+# #     #     normal_ll(a[b], mean = new_a[b], sd = 0.5) +
+# #     #     normal_ll(s[b], mean = new_s[b], sd = 0.5) -
+# #     #     (normal_ll(new_a[b], mean = a[b], sd = 0.5) +
+# #     #        normal_ll(new_s[b], mean = s[b], sd = 0.5))
+# #     # }
+# #     #
+# #     # print(new_score)
+# #     # print(curr_score)
+# #     # print(detailed_balance_part)
+# #
+# #     acceptance_score <- new_score - curr_score #+ detailed_balance_part
+# #     acceptance_prob <- exp(acceptance_score)
+# #
+# #     print(acceptance_prob)
+# #
+# #     u <- runif(1, 0, 1)
+# #     if(u < acceptance_prob){
+# #       mu <- new_mu
+# #       a <- new_a
+# #       std_dev <- new_std_dev
+# #       s  <- new_s
+# #       accepted[r] <- 1
+# #     }
+# #
+# #     weights <- N_k / sum(N_k)
+# #     N_k <- rep(0, K)
+# #     for(n in 1:N){
+# #       x <- X[n]
+# #       b_n <- b[n]
+# #       probs <- rep(0,K)
+# #       for(k in 1:K){
+# #         probs[k] <- weights[k] * (dnorm(x, mean = mu[k] + a[b_n], sd = std_dev[k] * s[b_n]) + 1e-12)
+# #       }
+# #
+# #       probs <- probs/sum(probs)
+# #
+# #       u <- runif(1, 0, 1)
+# #       c[n] <- c_n <- 1 + sum(u > cumsum(probs))
+# #       N_k[c_n] <- N_k[c_n] + 1
+# #
+# #     }
+# #
+# #     mu_saved[[r]] <- mu
+# #     a_saved[[r]] <- a
+# #     std_dev_saved[[r]] <- std_dev
+# #     s_saved[[r]] <- s
+# #     c_saved[[r]] <- c
+# #     weights_saved[[r]] <- weights
+# #     acceptance_probs[r] <- acceptance_prob
+# #   }
+# #
+# #   list(
+# #     "mu" = mu_saved,
+# #     "a" = a_saved,
+# #     "std_dev" = std_dev_saved,
+# #     "s" = s_saved,
+# #     "c" = c_saved,
+# #     "weights" = weights_saved,
+# #     "accepted" = accepted,
+# #     "acceptance_probs" = acceptance_probs
+# #   )
+# #
+# # }
+# 
+# 
+# generateBatchData <- function(N, P, cluster_means, std_dev, batch_shift, batch_var, cluster_weights, batch_weights,
+#                               row_names = paste0("Person_", 1:n),
+#                               col_names = paste0("Gene_", 1:p)) {
+# 
+#   # The number of distirbutions to sample from
+#   K <- length(cluster_means)
+# 
+#   B <- length(batch_shift)
+# 
+#   # The membership vector for the n points
+#   cluster_IDs <- sample(K, N, replace = T, prob = cluster_weights)
+# 
+#   batch_IDs <- sample(1:B, N, replace = T, prob = batch_weights)
+# 
+#   # The data matrix
+#   my_data <- my_corrected_data <- matrix(nrow = N, ncol = P)
+# 
+#   # Iterate over each of the columns permuting the means associated with each
+#   # label.
+#   for (p in 1:P)
+#   {
+#     reordered_cluster_means <- sample(cluster_means)
+#     reordered_std_devs <- sample(std_dev)
+#     
+#     reordered_batch_shift <- sample(batch_shift)
+#     reordered_batch_var <- sample(batch_var)
+# 
+#     # Draw n points from the K univariate Gaussians defined by the permuted means.
+#     for (n in 1:N) {
+# 
+#       # point_mean <- reordered_cluster_means[cluster_IDs[n]] + batch_shift[batch_IDs[n]]
+#       # point_sd <- std_dev * batch_var
+#       
+#       x <- rnorm(1)
+# 
+#       my_corrected_data[n, p] <- x * reordered_std_devs[cluster_IDs[n]] + reordered_cluster_means[cluster_IDs[n]]
+#       my_data[n, p] <- x * reordered_std_devs[cluster_IDs[n]] * reordered_batch_var[batch_IDs[n]] + reordered_cluster_means[cluster_IDs[n]] + reordered_batch_shift[batch_IDs[n]]
+#     }
+#   }
+# 
+#   # Order based upon allocation label
+#   row_order <- order(cluster_IDs)
+# 
+#   # Assign rownames and column names
+#   rownames(my_data) <- row_names
+#   colnames(my_data) <- col_names
+# 
+#   rownames(my_corrected_data) <- row_names
+#   colnames(my_corrected_data) <- col_names
+# 
+#   # Return the data and the allocation labels
+#   list(
+#     data = my_data[row_order, ],
+#     corrected_data = my_corrected_data[row_order, ],
+#     cluster_IDs = cluster_IDs[row_order],
+#     batch_IDs = batch_IDs[row_order]
+#   )
+# }
 
 # === Data generation ==========================================================
 set.seed(1)
 
 N <- 200
 P <- 2
-K <- 2
-B <- 5
+K <- 4
+B <- 7
 mean_dist <- 5
 batch_dist <- 2
 cluster_means <- 1:K * mean_dist
--- R/generateBatchData.R
+++ R/generateBatchData.R
@@ -11,20 +11,22 @@ generateBatchData <- function(N, P,
                               batch_weights,
                               row_names = paste0("Person_", 1:n),
                               col_names = paste0("Gene_", 1:p)) {
-
-  # The number of clusters and batches to generate
+  
+  # The number of distirbutions to sample from
   K <- length(cluster_means)
+  
   B <- length(batch_shift)
-
-  # The cluster and batch membership vector for the N points
+  
+  # The membership vector for the n points
   cluster_IDs <- sample(K, N, replace = T, prob = cluster_weights)
+  
   batch_IDs <- sample(1:B, N, replace = T, prob = batch_weights)
-
+  
   # The data matrix
   my_data <- my_corrected_data <- matrix(nrow = N, ncol = P)
-
-  # Iterate over each of the columns permuting the parameters associated with
-  # each label.
+  
+  # Iterate over each of the columns permuting the means associated with each
+  # label.
   for (p in 1:P)
   {
     reordered_cluster_means <- sample(cluster_means)
@@ -32,35 +34,32 @@ generateBatchData <- function(N, P,
     
     reordered_batch_shift <- sample(batch_shift)
     reordered_batch_var <- sample(batch_var)
-
+    
+    # Draw n points from the K univariate Gaussians defined by the permuted means.
     for (n in 1:N) {
       
-      # Draw the cluster defined univariate Gaussian.
-      my_corrected_data[n, p] <- x <- rnorm(1,
-        mean = reordered_cluster_means[cluster_IDs[n]],
-        sd = reordered_std_devs[cluster_IDs[n]]
-      )
+      # Draw a point from a standard normal
+      x <- rnorm(1)
       
-      # Add batch specific noise to the data
-      my_data[n, p] <- x + rnorm(1,
-        mean = reordered_batch_shift[batch_IDs[n]],
-        sd = reordered_batch_var[batch_IDs[n]]
-      )
+      # Adjust to the cluster distribution
+      my_corrected_data[n, p] <- x * reordered_std_devs[cluster_IDs[n]] + reordered_cluster_means[cluster_IDs[n]]
+      
+      # Adjust to the batched cluster distribution
+      my_data[n, p] <- x * reordered_std_devs[cluster_IDs[n]] * reordered_batch_var[batch_IDs[n]] + reordered_cluster_means[cluster_IDs[n]] + reordered_batch_shift[batch_IDs[n]]
     }
   }
-
+  
   # Order based upon allocation label
   row_order <- order(cluster_IDs)
-
+  
   # Assign rownames and column names
   rownames(my_data) <- row_names
   colnames(my_data) <- col_names
-
+  
   rownames(my_corrected_data) <- row_names
   colnames(my_corrected_data) <- col_names
-
-  # Return the data, the data without batch effects, and the allocation and 
-  # batch labels
+  
+  # Return the data and the allocation labels
   list(
     data = my_data[row_order, ],
     corrected_data = my_corrected_data[row_order, ],
