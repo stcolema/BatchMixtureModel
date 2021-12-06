8352201ebdd02e7f5a0b8f42831f67b5ea2162cd
Stephen Coleman <34100773+stcolema@users.noreply.github.com>
2021-12-06 13:18:56 -0500

    Updated as ``runMCMCChains`` now records chain number. Option to colour lines by chain.

--- R/plotLikelihoods.R
+++ R/plotLikelihoods.R
@@ -5,11 +5,13 @@
 #' @param choice The model fit score to use. Must be one of
 #' ``'observed_likelihood'``, ``'complete_likelihood'`` or ``'BIC'``. Defaults
 #' to ``'complete_likelihood'``.
+#' @param colour_by_chain Logical indcating if plots should be coloured by chain
+#' or all the same colour. Defaults to ``TRUE``.
 #' @return A ggplot2 object. Line plot of likelihood across iteration.
 #' @importFrom ggplot2 ggplot aes_string geom_line
 #' @export
 #' @examples
-#'
+#' 
 #' # Data in a matrix format
 #' X <- matrix(c(rnorm(100, 0, 1), rnorm(100, 3, 1)), ncol = 2, byrow = TRUE)
 #'
@@ -35,16 +37,33 @@
 #' samples <- runMCMCChains(X, n_chains, R, thin, labels, fixed, batch_vec, "MVN")
 #'
 #' p <- plotLikelihoods(samples)
-plotLikelihoods <- function(mcmc_outputs, choice = "complete_likelihood") {
+#' 
+plotLikelihoods <- function(mcmc_outputs, 
+                            choice = "complete_likelihood",
+                            colour_by_chain = TRUE
+                            ) {
   lkl_lst <- lapply(mcmc_outputs, getLikelihood, choice)
 
   n_chains <- length(lkl_lst)
-  for (ii in seq(n_chains)) {
-    lkl_lst[[ii]]$Chain <- ii
+  for(ii in seq(1, n_chains)) {
+    lkl_lst[[ii]]$Chain <- mcmc_outputs[[ii]]$Chain
   }
 
   lkl_df <- do.call(rbind, lkl_lst)
+  lkl_df$Chain <- factor(lkl_df$Chain)
 
+  if(colour_by_chain) {
+    
+    p <- ggplot2::ggplot(
+      data = lkl_df,
+      mapping = ggplot2::aes_string(
+        x = "iteration",
+        y = choice,
+        colour = "Chain"
+      )
+    ) +
+      ggplot2::geom_line()
+  } else {
   p <- ggplot2::ggplot(
     data = lkl_df,
     mapping = ggplot2::aes_string(
@@ -54,6 +73,6 @@ plotLikelihoods <- function(mcmc_outputs, choice = "complete_likelihood") {
     )
   ) +
     ggplot2::geom_line()
-
+}
   p
 }
